da49f03642c60770a57355fc18347122e3fd9395
yujingwyh <yujingwyh@163.com>
2019-10-29 11:24:07 +0800

    6.0.0版本

--- es/utils/throwError.d.ts
+++ es/utils/throwError.d.ts
@@ -3,4 +3,4 @@
  * @param condition {boolean} 条件
  * @param message {string} 错误信息
  */
-export default function throwError(condition: boolean, message: string): void;
+export default function throwError(condition: any, message: string): void;
--- src/utils/throwError.ts
+++ src/utils/throwError.ts
@@ -3,7 +3,7 @@
  * @param condition {boolean} 条件
  * @param message {string} 错误信息
  */
-export default function throwError(condition: boolean, message: string): void {
+export default function throwError(condition: any, message: string): void {
   if (condition) {
     throw new Error(message)
   }
